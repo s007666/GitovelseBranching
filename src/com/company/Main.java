8ecc0ef7267f5fe2e2153ac28ea48cdba33a1a14
s007744 <s007744@dtu.dk>
2021-09-27 12:52:34 +0200

    Commit A2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// A2 Status for projectet ændret til Public så alle kan se det!
     //A1
 
 
